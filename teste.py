0b28558acb5dc94bee3b6fa9b636c2f161900a3f
Gabrielle Desiree <53282226+4gabid@users.noreply.github.com>
2023-07-13 16:00:02 -0300

    Update teste.py
    c[odigo

--- teste.py
+++ teste.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.title("Execute um aplicativo Streamlit no Google Colab Notebook")
+st.subheader('By Gabrielle Desireé')
+
+x= st.slider("X", 1,20, 1)
+chart_data = pd.DataFrame(
+    np.random.randn(x, 3),
+    columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)
